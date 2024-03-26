04110bbe6c85f8c23cf77ed1d23e567c5514833e
Egor <yp27062004@mail.ru>
2024-03-26 15:11:59 +0300

    MyFirstShopSite

---
+++ main/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render 
+
+def index(request):
+    context = {
+        'title' : 'Home',
+        'content' : 'Главная страница магазина "Home"',
+        'list' : ['first','second'],
+        'dict' : {'first' : 1},
+        'is_authentificated' : False,
+    }
+    
+    return render(request,'main/index.html',context)
+
+def about(request):
+    return render(request,)
+
+
+
+    
